ee5c139ceb5953f4e12c66de918953bd657d534b
Kshitij <kshitijsaxena734@gmail.com>
2025-12-18 12:24:52 +0100

    Artifacts and Log removed from the github repo

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -30,9 +30,9 @@ import dagshub
 dagshub.init(repo_owner='Kshitij-AI-Architect', repo_name='Network-Security', mlflow=True)
 
 
-# os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/krishnaik06/networksecurity.mlflow"
-# os.environ["MLFLOW_TRACKING_USERNAME"]="krishnaik06"
-# os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ece21e0e2adb4e36a250ae3251f"
+# os.environ["MLFLOW_TRACKING_URI"]=""
+# os.environ["MLFLOW_TRACKING_USERNAME"]=""
+# os.environ["MLFLOW_TRACKING_PASSWORD"]=""
 
 
 class ModelTrainer:
