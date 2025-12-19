e2c391267bb23b482528e1f9be6c7eb34e93aa1b
Kshitij <kshitijsaxena734@gmail.com>
2025-12-20 00:33:25 +0100

    Training Pipeline completed using Swagger API endpoints

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -133,6 +133,8 @@ class ModelTrainer:
         network_model = NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(file_path=self.model_trainer_config.trained_model_file_path,obj=network_model)
         
+        save_object = ("final_model/model.pkl", best_model)
+        
         #Model Trainer Artifact
         model_trainer_artifact=ModelTrainerArtifact(
             trained_model_file_path=self.model_trainer_config.trained_model_file_path,
